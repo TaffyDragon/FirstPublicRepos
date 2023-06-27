f59d632cad549ab3a076a866d9cfe15030f581f2
Heather Ballard <124806065+TaffyDragon@users.noreply.github.com>
2023-06-27 13:37:17 -0400

    Add files via upload

---
+++ inputTest.py
@@ -0,0 +1,8 @@
+var1 = input ("Type something here.")
+var2 = input ("Type something else here.")
+
+print("The first thing you typed in was: ")
+print (var1)
+print ("The second thing you typed in was: ")
+print (var2)
+print ("Done!")
